366b34d2d68e12208cd9b4ab35033a2925f69ddc
neeraj-gs <gsneeraj05@gmail.com>
2024-02-10 08:51:09 +0530

    Started with a Event Management Smart Contract and initalised basic state varaibles

---
+++ EventOrg.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract EventTicket{
+    uint public numberofTickets;
+    uint public ticketPrice;
+    uint public totalAmount;
+    uint public startAt;
+    uint public endAt;
+    uint public timestamp;
+    string public message = "Buy Your First Event Ticket";
+}
