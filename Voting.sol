5cb3a8cec7b5c8ece7b879513477eeaad27435a9
neeraj-gs <gsneeraj05@gmail.com>
2024-02-12 10:04:44 +0530

    Added  Voting smart contrct notes and project omplementations

--- Voting.sol
+++ Voting.sol
@@ -2,3 +2,17 @@
 pragma solidity ^0.8.0;
 
 //Voting Smart Contract
+
+contract Vote{
+    address electionCommision;
+    address public winner;
+
+    struct Voter{
+        string name;
+        uint age;
+        uint voterId;
+        string gender;
+        uint voteCandidateId;
+        address voterAddress;
+    }
+}
