296b715e3156b39bf03aeeac1071f6fba926a468
neeraj-gs <gsneeraj05@gmail.com>
2024-02-12 10:09:06 +0530

    Added a Basic skeleton and constrcutor for the contract

--- Voting.sol
+++ Voting.sol
@@ -12,7 +12,33 @@ contract Vote{
         uint age;
         uint voterId;
         string gender;
-        uint voteCandidateId;
+        uint voteCandidateId; //the person to whom voter has voted
         address voterAddress;
     }
+
+    struct Candidate{
+        string name;
+        string party;
+        uint age;
+        string gender;
+        uint candidateId;
+        address candidateAddress;
+        uint votes;
+    }
+
+    uint nextVoterId = 1; //Voted Id for Each Voter, initially started with 1
+    uint candidateId = 1; //Candidate Id for candidates
+
+    uint startTime;
+    uint endTime;
+
+    mapping (uint => Voter) voterDetails; //Details of voters
+    mapping (uint => Candidate) candidateDetails;
+    bool stopVoting; //this is for some emergency situation to stop election
+
+
+    constructor(){
+        electionCommision=msg.sender; //assigning deployer as teh election commisoin
+    }
+    
 }
