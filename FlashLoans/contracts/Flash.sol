8106fcc7ff427e107f05950b822621dd8f18648a
neeraj-gs <gsneeraj05@gmail.com>
2024-02-21 18:14:30 +0530

    Added diffrent variables required for the flashloan and added addres of each of the contract and token

--- FlashLoans/contracts/Flash.sol
+++ FlashLoans/contracts/Flash.sol
@@ -1,3 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+// Uniswap interface and library imports
+import "./interface/IUniswapV2Factory.sol";
+import "./interface/IUniswapV2Pair.sol";
+import "./interface/IUniswapV2Router01.sol";
+import "./interface/IUniswapV2Router02.sol";
+import "./interface/IERC20.sol";
+import "./library/UniswapV2Library.sol";
+import "./library/SafeERC20.sol";
+import "hardhat/console.sol";
+
+contract FlashLoan {
+
+    // Factory and Routing Addresses
+    address private constant PANCAKE_FACTORY =
+        0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73;
+    address private constant PANCAKE_ROUTER =
+        0x10ED43C718714eb63d5aA57B78B54704E256024E;
+
+
+    // Token Addresses
+    address private constant BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
+    address private constant WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address private constant CROX = 0x2c094F5A7D1146BB93850f629501eB749f6Ed491;
+    address private constant CAKE = 0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82;
+
+    uint256 private deadline = block.timestamp + 1 days; //1 day is the deadline
+    uint256 private constant MAX_INT =
+        115792089237316195423570985008687907853269984665640564039457584007913129639935;
+}
