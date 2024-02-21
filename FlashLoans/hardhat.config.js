7e260e21d38d674387a9ae9322ca3f98611e3d71
neeraj-gs <gsneeraj05@gmail.com>
2024-02-21 17:49:39 +0530

    Added diffrent solidity versions and added a forked Binance public rpx nodes for the provider

--- FlashLoans/hardhat.config.js
+++ FlashLoans/hardhat.config.js
@@ -1,6 +1,21 @@
+const { version } = require("hardhat");
+
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers:[
+      {version:"0.5.5"},
+      {version:"0.6.6"},
+      {version:"0.8.8"},
+  ],
+  },
+  networks:{
+    hardhat:{
+      forking:{
+        url:"https://bsc-dataseed.binance.org/"
+      }
+    }
+  }
 };
