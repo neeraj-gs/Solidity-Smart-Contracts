c3989345adb73e286249c6a5efd60bff591eb486
neeraj-gs <gsneeraj05@gmail.com>
2024-02-22 09:47:23 +0530

    ADdded intiotiate arbitrage function to inititate the arbitrage and make it happen

--- FlashLoans/contracts/Flash.sol
+++ FlashLoans/contracts/Flash.sol
@@ -12,6 +12,7 @@ import "./library/SafeERC20.sol";
 import "hardhat/console.sol";
 
 contract FlashLoan {
+    using SafeERC20 for IERC20;
 
     // Factory and Routing Addresses
     address private constant PANCAKE_FACTORY =
@@ -29,4 +30,34 @@ contract FlashLoan {
     uint256 private deadline = block.timestamp + 1 days; //1 day is the deadline
     uint256 private constant MAX_INT =
         115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+
+    function initiateArbitrage(address _busdBorrow, uint _amout){ //addres of the token we are borrowing ffrom [any token can be taken] and the amount to borrow
+        IERC20(BUSD).safeApprove(address(PANCAKE_ROUTER),MAX_INT); //makes sure we approve pancake router to spend token[as we execute flash loans , contract have to make sure flashloans run properly]
+        IERC20(CROX).safeApprove(address(PANCAKE_ROUTER),MAX_INT);
+        IERC20(CAKE).safeApprove(address(PANCAKE_ROUTER),MAX_INT);
+
+        address pair = IUniswapV2Factory(PANCAKE_FACTORY).getPair(
+            _busdBorrow, //deals with busd and wbnb 
+            WBNB
+        ); //we get the liquidity pool that does trading between the 2 coins
+        require(pair!=address(0),"Pool Does not Exist for the Current Pair");
+        address token0 = IUniswapV2Pair(pair).token0(); //added address of token 1
+        address token1 = IUniswapV2Pair(pair).token1(); //added addres fo token 2
+
+        uint amount0Out = _busdBorrow==token0?_amout:0; //after fetching the token we assign the amount of token to the amount out
+        uint amount0Out = _busdBorrow==token1?_amout:0; //busd is trafdered here 
+
+
+        bytes memory data = abi.encode(_busdBorrow,_amout,msg.sender); //we need to use the borrowed busd in flash loans, triggers a flah loan 
+        //pair is the contract liquidity where busd and wbnb trading happens
+        IUniswapV2Pair(pair).swap(amount0Out, amount1Out,address(this),data); //transfer busd inot the account of our contract
+
+    }
+
+
+
+
+
+
 }
