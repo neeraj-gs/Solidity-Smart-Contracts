2abbdd4544beb8ed6627f7fe5f2ddb5686b97d43
neeraj-gs <gsneeraj05@gmail.com>
2024-02-22 12:19:24 +0530

    Aded same code as pancaek swap and added weth and dai tokens

---
+++ Swap-Uniswap/AddressList.js
@@ -0,0 +1,15 @@
+//Continas a list fo addrses taht si cmmuniacting eith the smart contract 
+//fetch price between 2 tokens 
+
+const factoryAddr="0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"; //uniswap factory v2 address
+const routerAddr="0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"; //uniswap router address
+const fromAddr="0xa2881a90bf33f03e7a3f803765cd2ed5c8928dfb"; //1st token WETH
+const toAddr="0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11"; //2nd TOken DAI
+
+module.exports={
+    factoryAddr,
+    routerAddr,
+    fromAddr,
+    toAddr
+}
+
---
+++ Swap-Uniswap/PriceFetch.js
@@ -0,0 +1,53 @@
+//Fetch price of token
+
+const ethers = require("ethers");
+const {factoryAddr,
+    routerAddr,
+    fromAddr,
+    toAddr} = require("./AddressList.js")
+
+const {erc20ABI,factoryABI,routerABI,pairABI} = require("./Abi.js")
+
+//we need a provider to connect to blockchain
+
+const provider = new ethers.providers.JsonRpcProvider(
+    "https://bsc-dataseed1.binance.org/"
+)
+
+//contract factory instnce 
+const factInstance = new ethers.Contract(
+    factoryAddr,factoryABI,provider
+)
+
+//router instance
+const routerInstance = new ethers.Contract(
+    routerAddr,routerABI,provider
+)
+
+
+const priceFetch = async(amount)=>{
+    const token1 = new ethers.Contract(
+        fromAddr,erc20ABI,provider
+    )
+
+    const token2 = new ethers.Contract(
+        toAddr,erc20ABI,provider
+    )
+
+    const decimal1 = await token1.decimals();
+    const decimal2 = await token2.decimals();
+    const amountIn = ethers.utils.parseUnits(humanFormat,decimal1).toString();
+
+    //number busd trnadered, checks , based on busd  how many wbnb we get 
+    const amountsOut = await routerInstance.getAmountsOut(amountIn,[
+        fromAddr,
+        toAddr
+    ]);
+    const humanOutput = ethers.utils.formatUnits(
+        amountsOut[1].toString(),
+        decimal2
+    )
+    console.log(humanOutput);
+}
+humanFormat="100"; //100 BUSD exchnage , how many wbnb we get it , convert 100 BUSD INto wei 100* 10^18
+priceFetch();
